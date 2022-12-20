bfa6a7cc2f6567b5cdd3f63e57989d639d0fddf4
Alessandro Rocha de Oliveira <alessandrorochadeoliveira@hotmail.com>
2022-12-20 08:16:43 -0300

    Exemplo de template

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "home.html", context={
+        "nome":"Alessandro Oliveira",
+    })
+
+def contato(request):
+    return HttpResponse("CONTATO")
+
+def sobre(request):
+    return HttpResponse("SOBRE")
